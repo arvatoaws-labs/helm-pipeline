fc1c8e5c8023e169b1a7d28337ea4ecc46282f3b
Patrick Robinson <patrick.robinson@bertelsmann.de>
2020-05-11 14:45:46 +0200

    FIX
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

--- eksctl-scripts/update-nodegroups.sh
+++ eksctl-scripts/update-nodegroups.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+set -x
 # ENV Params:
 # CLUSTER_FILE
 # OPTIONS
 
 CLUSTER_NAME=$(cat $CLUSTER_FILE | yq r - metadata.name)
 COMMIT_ID=$(git rev-parse HEAD)
-# TODO change node group names
-sed -i 's/-v[0-9a-f]\+$/-v123test/g' $CLUSTER_FILE
+sed -i "s/-v[0-9a-f]\+$/-v$COMMIT_ID/g" $CLUSTER_FILE
 git add $CLUSTER_FILE
 git commit -m "Pipeline Node Upgrade"
 git push
 eksctl get nodegroup --cluster $CLUSTER_NAME
-eksctl create nodegroup -f $CLUSTER_FILE $OPTIONS
+eksctl create nodegroup -f $CLUSTER_FILE
 eksctl delete nodegroup --only-missing -f $CLUSTER_FILE $OPTIONS
