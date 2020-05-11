b14978b97bbde2a3bab52c5bdb36bd41f2f73316
Patrick Robinson <patrick.robinson@bertelsmann.de>
2020-05-11 15:59:49 +0200

    Remove
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

--- eksctl-scripts/update-nodegroups.sh
+++ eksctl-scripts/update-nodegroups.sh
@@ -7,9 +7,10 @@ set -x
 CLUSTER_NAME=$(cat $CLUSTER_FILE | yq r - metadata.name)
 COMMIT_ID=$(git rev-parse HEAD)
 sed -i "s/-v[0-9a-f]\+$/-v$COMMIT_ID/g" $CLUSTER_FILE
-git add $CLUSTER_FILE
-git commit -m "Pipeline Node Upgrade"
-git push
+# TODO: Is this sensible?
+# git add $CLUSTER_FILE
+# git commit -m "Pipeline Node Upgrade"
+# git push
 eksctl get nodegroup --cluster $CLUSTER_NAME
 eksctl create nodegroup -f $CLUSTER_FILE
 eksctl delete nodegroup --only-missing -f $CLUSTER_FILE $OPTIONS
