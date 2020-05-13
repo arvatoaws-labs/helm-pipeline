5bd74f0a5978e5f771ab3c0190fdae0245658a83
Patrick Robinson <patrick.robinson@bertelsmann.de>
2020-05-13 12:42:22 +0200

    FIX
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

--- eksctl-scripts/update-nodegroups.sh
+++ eksctl-scripts/update-nodegroups.sh
@@ -3,9 +3,10 @@ set -x
 # ENV Params:
 # CLUSTER_FILE
 # OPTIONS
+# COMMIT_ID Override
 
 CLUSTER_NAME=$(cat $CLUSTER_FILE | yq r - metadata.name)
-COMMIT_ID=$(git rev-parse HEAD)
+: "${COMMIT_ID:=$(git rev-parse HEAD)}"
 sed -i "s/-v[0-9a-f]\+$/-v$COMMIT_ID/g" $CLUSTER_FILE
 # TODO: Is this sensible?
 # git add $CLUSTER_FILE
