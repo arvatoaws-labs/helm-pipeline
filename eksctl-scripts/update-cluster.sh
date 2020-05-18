4896ba9642c819c65b94f5314964c46760c293d4
Patrick Robinson <patrick.robinson@bertelsmann.de>
2020-05-18 11:25:23 +0200

    Print current version
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

--- eksctl-scripts/update-cluster.sh
+++ eksctl-scripts/update-cluster.sh
@@ -3,4 +3,8 @@
 # CLUSTER_FILE
 # OPTIONS
 
+CLUSTER_NAME=$(cat $CLUSTER_FILE | yq r - metadata.name)
+CURRENT_VERSION=$(eksctl get cluster -o json -n $CLUSTER_NAME | jq ".[].Version")
+
+echo "Current Cluster Version: $CURRENT_VERSION"
 eksctl update cluster -f $CLUSTER_FILE $OPTIONS
