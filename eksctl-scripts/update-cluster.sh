1a44a8c925510ed88e4e549c27a379e67476b2b9
Patrick Robinson <patrick.robinson@bertelsmann.de>
2020-05-18 11:28:38 +0200

    Add additional Info
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

--- eksctl-scripts/update-cluster.sh
+++ eksctl-scripts/update-cluster.sh
@@ -3,6 +3,9 @@
 # CLUSTER_FILE
 # OPTIONS
 
+echo "eksctl version:"
+eksctl version --output json
+
 CLUSTER_NAME=$(cat $CLUSTER_FILE | yq r - metadata.name)
 CURRENT_VERSION=$(eksctl get cluster -o json -n $CLUSTER_NAME | jq ".[].Version")
 
