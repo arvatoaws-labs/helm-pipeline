801b2eb5608720d5b0795cb886c6579ad3d94f53
Patrick Robinson <patrick.robinson@bertelsmann.de>
2020-05-20 16:10:44 +0200

    FIX
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

--- eksctl-scripts/unsilence.sh
+++ eksctl-scripts/unsilence.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -v -X DELETE -u $ALERTMANAGER_USER:$ALERTMANAGER_PW "https://alertmanager-basic.prod.mgmt.aws-arvato.com/api/v2/silences/$(cat silence_id)" -H "accept: application/json"
+curl -v -X DELETE -u $ALERTMANAGER_USER:$ALERTMANAGER_PW "https://alertmanager-basic.prod.mgmt.aws-arvato.com/api/v2/silence/$(cat silence_id)" -H "accept: application/json"
