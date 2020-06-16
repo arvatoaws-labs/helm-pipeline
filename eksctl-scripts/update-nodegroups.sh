dea62a7cbb9392b0667ce8c76fd742c20455fab2
Patrick Robinson <patrick.robinson@bertelsmann.de>
2020-06-16 10:39:49 +0200

    Idea, not working yet
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

--- eksctl-scripts/update-nodegroups.sh
+++ eksctl-scripts/update-nodegroups.sh
@@ -7,11 +7,14 @@ set -x
 
 CLUSTER_NAME=$(cat $CLUSTER_FILE | yq r - metadata.name)
 : "${COMMIT_ID:=$(git rev-parse HEAD)}"
-sed -i "s/-v[0-9a-f]\+$/-v$COMMIT_ID/g" $CLUSTER_FILE
-# TODO: Is this sensible?
-# git add $CLUSTER_FILE
-# git commit -m "Pipeline Node Upgrade"
-# git push
-eksctl get nodegroup --cluster $CLUSTER_NAME
-eksctl create nodegroup -f $CLUSTER_FILE
-eksctl delete nodegroup --only-missing -f $CLUSTER_FILE $OPTIONS
+
+NUMBER_OF_NODEGROUPS=$(yq r $CLUSTER_FILE --length nodeGroups)
+for (( i=0; i<NUMBER_OF_NODEGROUPS; i++ )); do
+  NEW_NAME=$(yq r prod.yaml -j | jq ".nodeGroups[$i].name" | tr -d '"' | sed -i "s/-v[0-9a-f]\+$/-v$COMMIT_ID/g")
+  echo $NEW_NAME
+  yq r $CLUSTER_FILE -j | jq ".nodeGroups[$i].name = \"$NEW_NAME\"" | yq r --prettyPrint - > $CLUSTER_FILE
+  eksctl get nodegroup --cluster $CLUSTER_NAME
+  echo "eksctl create nodegroup -f $CLUSTER_FILE"
+  echo "eksctl delete nodegroup --only-missing -f $CLUSTER_FILE $OPTIONS"
+  sleep 60
+end
