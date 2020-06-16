906d6f03e20fe980dd22e2ba84bf6a8198f4465d
Patrick Robinson <patrick.robinson@bertelsmann.de>
2020-06-16 13:39:52 +0200

    FIX
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

--- eksctl-scripts/update-nodegroups.sh
+++ eksctl-scripts/update-nodegroups.sh
@@ -14,7 +14,7 @@ sed -i "s/-v[0-9a-f]\+$/-v$OLD_COMMIT_ID/g" $CLUSTER_FILE
 
 NUMBER_OF_NODEGROUPS=$(yq r $CLUSTER_FILE --length nodeGroups)
 for (( i=0; i<NUMBER_OF_NODEGROUPS; i++ )); do
-  NEW_NAME=$(yq r prod.yaml -j | jq ".nodeGroups[$i].name" | tr -d '"' | sed -e "s/-v[0-9a-f]\+$/-v$COMMIT_ID/g")
+  NEW_NAME=$(yq r $CLUSTER_FILE -j | jq ".nodeGroups[$i].name" | tr -d '"' | sed -e "s/-v[0-9a-f]\+$/-v$COMMIT_ID/g")
   yq r $CLUSTER_FILE -j | jq ".nodeGroups[$i].name = \"$NEW_NAME\"" | yq r --prettyPrint - | sed -e "s/ yes$/ \"yes\"/g" | sed -e "s/ no$/ \"no\"/g" > .tmpcopy
   mv .tmpcopy $CLUSTER_FILE
   eksctl create nodegroup -f $CLUSTER_FILE
