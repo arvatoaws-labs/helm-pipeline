0f366b51b9ec4c2d27280ce0d75d3a813d5c20d8
Patrick Robinson <patrick.robinson@bertelsmann.de>
2020-06-22 17:15:10 +0200

    FIX
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

--- eksctl-scripts/initialise-flux.sh
+++ eksctl-scripts/initialise-flux.sh
@@ -25,7 +25,7 @@ cat .tmpcurrentaws-auth
 kubectl apply -f .tmpcurrentaws-auth
 rm .tmpcurrentaws-auth
 
-if [ "$(update-or-create.sh)" == "true" ] && [ $(helm3 ls -n fluxcd | grep flux- | grep deployed | wc -l) -gt 0 ] && [ $(helm3 ls -n fluxcd | grep helm-operator | grep deployed | wc -l) -gt 0 ] && [ "$(helm3 get values -n fluxcd flux | yq r - prometheus.serviceMonitor.create)" == "true" ] && [ "$(helm3 get values -n fluxcd helm-operator | yq r - prometheus.serviceMonitor.create)" == "true" ] && [ $(helm3 get values -n fluxcd flux | yq r - additionalArgs --length) -gt 1 ] ; then
+if [ $(helm3 ls -n fluxcd | grep flux- | grep deployed | wc -l) -gt 0 ] && [ $(helm3 ls -n fluxcd | grep helm-operator | grep deployed | wc -l) -gt 0 ] && [ "$(helm3 get values -n fluxcd flux | yq r - prometheus.serviceMonitor.create)" == "true" ] && [ "$(helm3 get values -n fluxcd helm-operator | yq r - prometheus.serviceMonitor.create)" == "true" ] && [ $(helm3 get values -n fluxcd flux | yq r - additionalArgs --length) -gt 1 ] ; then
   echo "Service Monitors and Fluxcloud already integrated into flux"
   HELM_TOBE_REDONE="false"
 else
@@ -51,7 +51,7 @@ else
   helm3 upgrade -i helm-operator --version $HELM_OPERATOR_VERSION -f flux-helm-values/helm_operator.yaml --namespace fluxcd fluxcd/helm-operator
 fi
 sleep 60
-if [ "$(update-or-create.sh)" == "false" ]; then
+if [ "$(update-or-create.sh)" == "false" ]; then # TODO: Replace with check for upload? How to
   gh api -X POST repos/arvatoaws/$GIT_REPO/keys -F title="flux-$ENVIR" -F key="$(fluxctl identity --k8s-fwd-ns fluxcd)" -F read_only=false
 fi
 
