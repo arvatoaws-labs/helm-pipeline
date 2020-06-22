8fa14504bed7f45b42d1be2eb018ac2559a76642
Patrick Robinson <patrick.robinson@bertelsmann.de>
2020-06-22 16:23:57 +0200

    FIX
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

--- eksctl-scripts/initialise-flux.sh
+++ eksctl-scripts/initialise-flux.sh
@@ -6,11 +6,24 @@
 echo "eksctl version:"
 eksctl version --output json
 
+ACCOUNT=$(aws sts get-caller-identity | jq '.Account' | tr -d '"')
 CLUSTER_NAME=$(cat $CLUSTER_FILE | yq r - metadata.name)
 ENVIR_FILE=$(echo $CLUSTER_FILE | rev | cut -d '/' -f 1 | rev)
 ENVIR=$(echo $ENVIR_FILE | cut -d '.' -f 1)
 
 kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/$(cat base-templates/cert-manager/release.yaml | yq r - spec.chart.version)/cert-manager.crds.yaml
+
+kubectl get configmaps -n kube-system aws-auth -o yaml > .tmpcurrentaws-auth
+yq d -i .tmpcurrentaws-auth metadata.creationTimestamp
+yq d -i .tmpcurrentaws-auth metadata.resourceVersion
+yq d -i .tmpcurrentaws-auth metadata.selfLink
+yq d -i .tmpcurrentaws-auth metadata.uid
+NEW_CONTENT=$(cat .tmpcurrentaws-auth | yq r - data.mapRoles | yq r - -j | jq 'map({ (.rolearn): . }) | add' | jq ".\"arn:aws:iam::${ACCOUNT}:role/ToolsAcctCodePipelineKubernetesRole\" = {\"groups\":[\"system:masters\"],\"username\":\"codebuild\",\"rolearn\":\"arn:aws:iam::${ACCOUNT}:role/ToolsAcctCodePipelineKubernetesRole\"}" | jq ".\"arn:aws:iam::${ACCOUNT}:role/CsAdministratorRole\" = {\"groups\":[\"system:masters\"],\"username\":\"asyadmin\",\"rolearn\":\"arn:aws:iam::${ACCOUNT}:role/CsAdministratorRole\"}" | jq 'to_entries | map_values(.value)' | yq r - --prettyPrint)
+yq w -i -- .tmpcurrentaws-auth data.mapRoles "$NEW_CONTENT"
+echo "New aws-auth map:"
+cat .tmpcurrentaws-auth
+kubectl apply -f .tmpcurrentaws-auth
+rm .tmpcurrentaws-auth
 
 if [ "$(update-or-create.sh)" == "true" ] && [ $(helm3 ls -n fluxcd | grep flux- | grep deployed | wc -l) -gt 0 ] && [ $(helm3 ls -n fluxcd | grep helm-operator | grep deployed | wc -l) -gt 0 ] && [ "$(helm3 get values -n fluxcd flux | yq r - prometheus.serviceMonitor.create)" == "true" ] && [ "$(helm3 get values -n fluxcd helm-operator | yq r - prometheus.serviceMonitor.create)" == "true" ] && [ $(helm3 get values -n fluxcd flux | yq r - additionalArgs --length) -gt 1 ] ; then
   echo "Service Monitors and Fluxcloud already integrated into flux"
