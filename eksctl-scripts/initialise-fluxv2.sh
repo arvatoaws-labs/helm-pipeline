9e6783cf3e7af09cf9a3d37aad63f17d3a18ae92
Patrick Robinson <patrick.robinson@bertelsmann.de>
2021-02-18 13:28:45 +0100

    Remove unneeded arch
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

--- eksctl-scripts/initialise-fluxv2.sh
+++ eksctl-scripts/initialise-fluxv2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $(kubectl get ns | grep flux-system | wc -l) -gt 0 ] && [ $(kubectl get deployment -n flux-system | grep controller | wc -l) -gt 3 ]; then
-  flux install --arch $ARCH --version $FLUX_VERSION
+  flux install --version $FLUX_VERSION
 else
-  flux bootstrap github --arch $ARCH --owner=$GIT_OWNER --repository=$GIT_REPO --branch=$GIT_BRANCH --path=$BOOTSTRAP_DIR --version $FLUX_VERSION
+  flux bootstrap github --owner=$GIT_OWNER --repository=$GIT_REPO --branch=$GIT_BRANCH --path=$BOOTSTRAP_DIR --version $FLUX_VERSION
 fi
