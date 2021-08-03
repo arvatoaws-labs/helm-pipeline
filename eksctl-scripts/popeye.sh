307c970c772244b7a1a2d428c79ac12a5a20fe99
AlbertSchwarzkopf <albert.schwarzkopf@bertelsmann.de>
2021-08-03 13:14:42 +0200

    test fix

--- eksctl-scripts/popeye.sh
+++ eksctl-scripts/popeye.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [ "$(update-or-create.sh)" == "true" ]; then
-  popeye -l error -o junit > popeye-tests.xml
+  #popeye -l error -o junit > popeye-tests.xml
+  popeye -l error -o junit --output-file popeye-tests.xml
 else
   echo '<?xml version="1.0" encoding="UTF-8"?><testsuites name="Popeye" tests="0" failures="0" errors="0"></testsuites>' > popeye-tests.xml
 fi
