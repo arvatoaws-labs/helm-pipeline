42daba0118ced2ea642742ebf1f1b38280b1e995
AlbertSchwarzkopf <albert.schwarzkopf@bertelsmann.de>
2021-08-03 14:28:31 +0200

    rollback

--- eksctl-scripts/popeye.sh
+++ eksctl-scripts/popeye.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ "$(update-or-create.sh)" == "true" ]; then
-  #popeye -l error -o junit > popeye-tests.xml
-  POPEYE_REPORT_DIR=$(pwd) popeye -l error -o junit --save --output-file popeye-tests.xml
+  popeye -l error -o junit > popeye-tests.xml
+  #POPEYE_REPORT_DIR=$(pwd) popeye -l error -o junit --save --output-file popeye-tests.xml
 else
   echo '<?xml version="1.0" encoding="UTF-8"?><testsuites name="Popeye" tests="0" failures="0" errors="0"></testsuites>' > popeye-tests.xml
 fi
