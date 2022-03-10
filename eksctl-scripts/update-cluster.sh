f2c07df0adffeb97fc0288a9f315f4f5068aad26
Sven Meinking <64302328+svmnkg@users.noreply.github.com>
2022-03-10 15:13:10 +0100

    yq fix

--- eksctl-scripts/update-cluster.sh
+++ eksctl-scripts/update-cluster.sh
@@ -18,14 +18,14 @@ fi
 echo "eksctl version:"
 eksctl version --output json
 
-if [ "$(yq r $CLUSTER_FILE metadata.version)" == "auto" ]; then
+if [ "$(yq eval '.metadata.version' $CLUSTER_FILE )" == "auto" ]; then
   MAX_SUPPORTED_VERSION=$(eksctl version -o json | jq '.EKSServerSupportedVersions | map(tonumber) | max')
   echo "Setting autoversion to $MAX_SUPPORTED_VERSION for creation"
   sed -i "s/version:\ auto/version:\ \"$MAX_SUPPORTED_VERSION\"/g" $CLUSTER_FILE
   VERSION_OVERWRITEN="true"
 fi
 
-CLUSTER_NAME=$(cat $CLUSTER_FILE | yq r - metadata.name)
+CLUSTER_NAME=$(cat $CLUSTER_FILE | yq eval '.metadata.name' -)
 CURRENT_VERSION=$(eksctl get cluster -o json -n $CLUSTER_NAME | jq ".[].Version")
 
 echo "Current Cluster Version: $CURRENT_VERSION"
