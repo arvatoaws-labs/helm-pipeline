92778cf4705ec7c566c5bb1fc29ff0325bc5efc2
Sven Meinking <64302328+svmnkg@users.noreply.github.com>
2022-03-10 15:41:32 +0100

    fix yq

--- eksctl-scripts/oidc-setup.sh
+++ eksctl-scripts/oidc-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "$(cat $CLUSTER_FILE | yq r - iam.withOIDC)" == "true" ]]; then
+if [[ "$(cat $CLUSTER_FILE | yq eval '.iam.withOIDC' -)" == "true" ]]; then
   eksctl utils associate-iam-oidc-provider -f $CLUSTER_FILE | grep "already associated"
   if [[ $? -eq 1 ]]; then
     eksctl utils associate-iam-oidc-provider -f $CLUSTER_FILE --approve
