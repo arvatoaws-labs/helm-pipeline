3b4c444723335098ba874134ef166b408bd37437
Sven Meinking <64302328+svmnkg@users.noreply.github.com>
2022-03-10 15:33:20 +0100

    yq fix pt1

--- eksctl-scripts/setup-aws-access.sh
+++ eksctl-scripts/setup-aws-access.sh
@@ -6,10 +6,10 @@ echo "Account: $ACCOUNT"
 # Disabled as per Philipps request: kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/$(cat base-templates/cert-manager/release.yaml | yq r - spec.chart.version)/cert-manager.crds.yaml
 
 kubectl get configmaps -n kube-system aws-auth -o yaml > .tmpcurrentaws-auth
-yq d -i .tmpcurrentaws-auth metadata.creationTimestamp
-yq d -i .tmpcurrentaws-auth metadata.resourceVersion
-yq d -i .tmpcurrentaws-auth metadata.selfLink
-yq d -i .tmpcurrentaws-auth metadata.uid
+yq -i 'del(.metadata.creationTimestamp)' .tmpcurrentaws-auth
+yq -i 'del(.metadata.resourceVersion)' .tmpcurrentaws-auth
+yq -i 'del(.metadata.selfLink)' .tmpcurrentaws-auth
+yq -i 'del(.metadata.uid)' .tmpcurrentaws-auth
 NEW_CONTENT=$(cat .tmpcurrentaws-auth | yq r - data.mapRoles | yq r - -j | jq 'map({ (.rolearn): . }) | add' | jq ".\"arn:aws:iam::${ACCOUNT}:role/ToolsAcctCodePipelineKubernetesRole\" = {\"groups\":[\"system:masters\"],\"username\":\"codebuild\",\"rolearn\":\"arn:aws:iam::${ACCOUNT}:role/ToolsAcctCodePipelineKubernetesRole\"}" | jq ".\"arn:aws:iam::${ACCOUNT}:role/CsAdministratorRole\" = {\"groups\":[\"system:masters\"],\"username\":\"asyadmin\",\"rolearn\":\"arn:aws:iam::${ACCOUNT}:role/CsAdministratorRole\"}" | jq ".\"arn:aws:iam::${ACCOUNT}:role/CsAuditorRole\" = {\"groups\":[\"eks-console-dashboard-read-access-group\"],\"username\":\"asysauditor\",\"rolearn\":\"arn:aws:iam::${ACCOUNT}:role/CsAuditorRole\"}" | jq 'to_entries | map_values(.value)' | yq r - --prettyPrint)
 yq w -i -- .tmpcurrentaws-auth data.mapRoles "$NEW_CONTENT"
 echo "New aws-auth map:"
