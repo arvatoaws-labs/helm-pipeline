0b231f91b82986b688345e337fa1d679cbc8a810
Sven Meinking <64302328+svmnkg@users.noreply.github.com>
2022-03-22 11:01:51 +0100

    yq fix

--- eksctl-scripts/update-nodegroups.sh
+++ eksctl-scripts/update-nodegroups.sh
@@ -39,7 +39,7 @@ else
   OLD_COMMIT_ID=$(cut -d'v' -f 2 <<< $(eksctl get nodegroup --cluster $CLUSTER_NAME | grep $CLUSTER_NAME | awk '{ print $2; }' | head -n 1))
   sed -i "s/-v[0-9a-f]\+$/-v$OLD_COMMIT_ID/g" $CLUSTER_FILE
 
-  NUMBER_OF_NODEGROUPS=$(yq r $CLUSTER_FILE --length nodeGroups)
+  NUMBER_OF_NODEGROUPS=$(yq '.nodeGroups | length' $CLUSTER_FILE)
   for (( i=0; i<NUMBER_OF_NODEGROUPS; i++ )); do
     NEW_NAME=$(yq eval $CLUSTER_FILE -j | jq ".nodeGroups[$i].name" | tr -d '"' | sed -e "s/-v[0-9a-f]\+$/-v$COMMIT_ID/g")
     yq r $CLUSTER_FILE -j | jq ".nodeGroups[$i].name = \"$NEW_NAME\"" | yq r --prettyPrint - | sed -e "s/ yes$/ \"yes\"/g" | sed -e "s/ no$/ \"no\"/g" > .tmpcopy
