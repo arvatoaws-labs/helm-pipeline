05f27d4041265873925d19e08659f5797267986c
Patrick Robinson <patrick.robinson@bertelsmann.de>
2022-05-09 13:30:01 +0200

    Fix
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

--- eksctl-scripts/setup-aws-access.sh
+++ eksctl-scripts/setup-aws-access.sh
@@ -10,8 +10,9 @@ yq -i 'del(.metadata.creationTimestamp)' .tmpcurrentaws-auth
 yq -i 'del(.metadata.resourceVersion)' .tmpcurrentaws-auth
 yq -i 'del(.metadata.selfLink)' .tmpcurrentaws-auth
 yq -i 'del(.metadata.uid)' .tmpcurrentaws-auth
+yq -i 'del(.metadata.annotations["kubectl.kubernetes.io/last-applied-configuration"])' .tmpcurrentaws-auth
 NEWCONTENT=$(cat .tmpcurrentaws-auth | yq '.data.mapRoles' - | yq -o=json - | jq 'map({ (.rolearn): . }) | add' | jq ".\"arn:aws:iam::${ACCOUNT}:role/ToolsAcctCodePipelineKubernetesRole\" = {\"groups\":[\"system:masters\"],\"username\":\"codebuild\",\"rolearn\":\"arn:aws:iam::${ACCOUNT}:role/ToolsAcctCodePipelineKubernetesRole\"}" | jq ".\"arn:aws:iam::${ACCOUNT}:role/CsAdministratorRole\" = {\"groups\":[\"system:masters\"],\"username\":\"asyadmin\",\"rolearn\":\"arn:aws:iam::${ACCOUNT}:role/CsAdministratorRole\"}" | jq ".\"arn:aws:iam::${ACCOUNT}:role/CsAuditorRole\" = {\"groups\":[\"eks-console-dashboard-read-access-group\"],\"username\":\"asysauditor\",\"rolearn\":\"arn:aws:iam::${ACCOUNT}:role/CsAuditorRole\"}" | jq 'to_entries | map_values(.value)' | yq -P -)
-yq -i '.data.mapRoles = $NEWCONTENT' .tmpcurrentaws-auth
+yq -i ".data.mapRoles = \"$NEWCONTENT\"" .tmpcurrentaws-auth
 echo "New aws-auth map:"
 cat .tmpcurrentaws-auth
 kubectl apply -f .tmpcurrentaws-auth
